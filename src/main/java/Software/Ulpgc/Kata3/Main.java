55d0534fb7a84ce61493108fd2bc63f8fc5a836b
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2023-12-30 17:06:09 +0000

    Main class modified to create histogram

--- src/main/java/Software/Ulpgc/Kata3/Main.java
+++ src/main/java/Software/Ulpgc/Kata3/Main.java
@@ -10,12 +10,20 @@ public class Main {
         ComputerLoader loader = new TsvFileComputerLoader(new File("laptops_train.txt"));
         List<Computer> computers = loader.load();
         Map<String, Integer> chart = new HashMap<>();
+        double[] prices = new double[1000];
+        int i = 0;
         for (Computer computer:computers) {
+            prices[i++] = Double.valueOf(computer.getPrice())*0.000108;
             String manufacturer = computer.getManufacturer();
             chart.put(manufacturer, chart.getOrDefault(manufacturer, 0)+1);
         }
         for (String key : chart.keySet()) {
             System.out.println(key + " : " + chart.get(key));
         }
+
+        Histogram histogram = new Histogram("Prices of computers", "Prices in euros", "y", prices, 5);
+        MainFrame frame = new MainFrame();
+        frame.histogramDisplay().show(histogram);
+        frame.setVisible(true);
     }
 }
